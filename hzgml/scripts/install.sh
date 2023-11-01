2ddb6b052f669fcbc964549869a1e0dd8406c9dc
JiehanHe <jiehan@lxplus772.cern.ch>
2023-11-01 13:37:42 +0100

    update of setting up hzgml environment

--- hzgml/scripts/install.sh
+++ hzgml/scripts/install.sh
@@ -1,10 +1,11 @@
 # include ROOT
-source /cvmfs/sft.cern.ch/lcg/views/LCG_100/x86_64-centos7-gcc9-opt/setup.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_100/ROOT/v6.24.00/x86_64-centos7-gcc9-opt/bin/thisroot.sh
-python3 -m venv /eos/user/j/jiehan/hzgmlenv
-source /eos/user/j/jiehan/hzgmlenv/bin/activate
+pythonpath=/eos/user/j/jiehan/hzgmlenv_test
+source "$pythonpath/bin/activate"
 pip install --upgrade pip
 pip install -r requirements.txt
+
+source /cvmfs/sft.cern.ch/lcg/views/LCG_100/x86_64-centos7-gcc9-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_100/ROOT/v6.24.00/x86_64-centos7-gcc9-opt/bin/thisroot.sh
 
 # setting python path
 export PATH="`pwd`/scripts:${PATH}"
