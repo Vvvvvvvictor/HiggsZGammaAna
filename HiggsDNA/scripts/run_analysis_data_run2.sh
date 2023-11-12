0873c7d6ac78c2963ec71a8fb3e224b19ce7b17a
zewang <zebing.wang@cern.ch>
2023-11-12 19:37:23 +0100

    add plot codes

--- HiggsDNA/scripts/run_analysis_data_run2.sh
+++ HiggsDNA/scripts/run_analysis_data_run2.sh
@@ -1,3 +1,3 @@
-outdir="/afs/cern.ch/work/z/zewang/private/HZGamma/data_run2_test"
+outdir="/afs/cern.ch/work/z/zewang/private/HZGamma/data_run2_2017"
 
-python scripts/run_analysis.py --config "metadata/zgamma_data_run2.json" --log-level "DEBUG" --n_cores 10 --output_dir $outdir --unretire_jobs --batch_system "local" --short
+python scripts/run_analysis.py --config "metadata/zgamma_data_run2.json" --log-level "DEBUG" --n_cores 10 --output_dir $outdir --unretire_jobs #--batch_system "local" --short
--- HiggsDNA/scripts/run_analysis_signal_run2.sh
+++ HiggsDNA/scripts/run_analysis_signal_run2.sh
@@ -1,4 +1,4 @@
-outdir="/afs/cern.ch/work/z/zewang/private/HZGamma/signal_run2"
+outdir="/afs/cern.ch/work/z/zewang/private/HZGamma/signal_run2_2017"
 
 python scripts/run_analysis.py --config "metadata/zgamma_signal_run2.json" --log-level "DEBUG" --n_cores 10 --output_dir $outdir --unretire_jobs --batch_system "local" #--short
 
