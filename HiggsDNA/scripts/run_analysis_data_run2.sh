42b15d2d88f4477f9a2987618d26ddb2caf49519
JiehanHe <jiehan@lxplus737.cern.ch>
2023-11-19 09:06:39 +0100

    newest json

--- HiggsDNA/scripts/run_analysis_data_run2.sh
+++ HiggsDNA/scripts/run_analysis_data_run2.sh
@@ -1,3 +1,3 @@
 outdir="/eos/home-j/jiehan/parquet/2017/nanov9/data/"
 
-python scripts/run_analysis.py --config "metadata/zgamma_data_run2.json" --log-level "DEBUG" --n_cores 10 --output_dir $outdir --unretire_jobs --batch_system "local" #--short
+python scripts/run_analysis.py --config "metadata/zgamma_data_run2.json" --log-level "DEBUG" --n_cores 5 --output_dir $outdir --unretire_jobs --batch_system "local" #--short
