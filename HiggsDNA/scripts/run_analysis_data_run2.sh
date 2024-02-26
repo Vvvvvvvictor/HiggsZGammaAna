0957fcc96773d7e8b8870bcf063d38512c6f9ae8
Mingtao <mtzhang@stu.pku.edu.cn>
2024-02-26 10:53:25 +0100

    origin

--- HiggsDNA/scripts/run_analysis_data_run2.sh
+++ HiggsDNA/scripts/run_analysis_data_run2.sh
@@ -1,3 +1,3 @@
-outdir="/eos/home-j/jiehan/parquet/nanov9/data"
+outdir="/eos/user/m/mingtao/workspace/zgamma/parquet/data_run2"
 
-python scripts/run_analysis.py --config "metadata/zgamma_data_run2.json" --log-level "DEBUG" --n_cores 10 --output_dir $outdir --batch_system "condor" --unretire_jobs #--short 
+python scripts/run_analysis.py --config "metadata/zgamma_data_run2.json" --log-level "DEBUG" --n_cores 10 --output_dir $outdir --batch_system "local" --unretire_jobs --merge_outputs #--short 
