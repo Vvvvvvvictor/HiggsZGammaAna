9f9ca9727f7cbbb245d15a012033b0116dc76d33
zewang <zebing.wang@cern.ch>
2024-09-19 16:09:28 +0200

    sys update

--- HiggsDNA/scripts/run_analysis_signal_run2.sh
+++ HiggsDNA/scripts/run_analysis_signal_run2.sh
@@ -1,3 +1,3 @@
 outdir="/afs/cern.ch/work/z/zewang/private/HZGamma/test2"
 
-python scripts/run_analysis.py --config "metadata/zgamma_signal_run2.json" --log-level "DEBUG" --n_cores 10 --output_dir $outdir --unretire_jobs --batch_system "local" #--short
+python scripts/run_analysis.py --config "metadata/zgamma_signal_run2.json" --log-level "DEBUG" --n_cores 10 --output_dir $outdir --unretire_jobs --batch_system "local" --short
