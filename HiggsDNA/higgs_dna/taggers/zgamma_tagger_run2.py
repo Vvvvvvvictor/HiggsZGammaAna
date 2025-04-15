ade765928ddeab3961ed82f646669a17c3d1bf52
shsong <shaowei.song@cern.ch>
2025-04-15 16:44:00 +0200

    HEM

--- HiggsDNA/higgs_dna/taggers/zgamma_tagger_run2.py
+++ HiggsDNA/higgs_dna/taggers/zgamma_tagger_run2.py
@@ -554,7 +554,26 @@ class ZGammaTaggerRun2(Tagger):
         mass_cut = (z_cands.ZCand.mass > 80.) & (z_cands.ZCand.mass < 100.)
         # mass_cut = z_cands.ZCand.mass > 50.
         z_cands = z_cands[mass_cut] # OSSF lepton pairs with m_ll > 50.
-
+        
+        # HEM cut
+        if self.year=="2018" and self.is_data:
+            hem_run=events.run > 319077        
+            # checked 65.15623538907509% events in data could pass this run cut
+            hem_jet=awkward.num(events.Jet[(events.Jet.phi>-1.57) & (events.Jet.phi<-0.87) & (events.Jet.eta>-3) & (events.Jet.eta<-1.3)])>0
+            hem_fatjet=awkward.num(events.FatJet[(events.FatJet.phi>-1.57) & (events.FatJet.phi<-0.87) & (events.FatJet.eta>-3) & (events.FatJet.eta<-1.3)])>0
+            hem_cut=~((hem_run & hem_jet) | (hem_run & hem_fatjet))        
+        elif self.year=="2018" and not self.is_data:
+            #random number generator from 0 to 1
+            fraction=0.6515623538907509
+            events['random'] = numpy.random.rand(len(events))
+            hem_run=events.random < fraction
+            hem_jet=awkward.num(events.Jet[(events.Jet.phi>-1.57) & (events.Jet.phi<-0.87) & (events.Jet.eta>-3) & (events.Jet.eta<-1.3)])>0
+            hem_fatjet=awkward.num(events.FatJet[(events.FatJet.phi>-1.57) & (events.FatJet.phi<-0.87) & (events.FatJet.eta>-3) & (events.FatJet.eta<-1.3)])>0
+            hem_cut=~((hem_run & hem_jet) | (hem_run & hem_fatjet))
+        else:
+            hem_cut=awkward.num(events.Photon) >= 0 
+        events = events[hem_cut]
+        
         # # Construct di-electron/di-muon pairs
         # ee_pairs = awkward.combinations(electrons, 2, fields = ["LeadLepton", "SubleadLepton"])
         # if self.year is not None:
