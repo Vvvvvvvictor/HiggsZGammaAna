a5eec44cdb942f8c6895a946aa4df93098e3dbbe
Pei-Zhu Lai <ht910155@gmail.com>
2026-03-31 07:03:45 +0200

    to remove record file

--- HiggsDNA/scripts/run_analysis_signal_run3.sh
+++ HiggsDNA/scripts/run_analysis_signal_run3.sh
@@ -1,5 +1,8 @@
 # outdir="/eos/home-j/jiehan/parquet/nanov12/signal"
 outdir="/eos/home-p/pelai/HZgamma/parquet_DNA/run3/signal"
 
+rm -fr /eos/home-p/pelai/HZgamma/parquet_DNA/run3/signal/ggH_M125_2023postBPix
+rm -fr /eos/home-p/pelai/HZgamma/parquet_DNA/run3/signal/analysis_manager.pkl
+
 python scripts/run_analysis.py --config "metadata/zgamma_signal_run3.json" --log-level "DEBUG" --n_cores 10 --output_dir $outdir --unretire_jobs --batch_system "local" #--short #--with_skimmed #condor #local
 # python scripts/run_analysis.py --config "metadata/zgamma_signal_run3_ext.json" --log-level "DEBUG" --n_cores 10 --output_dir $outdir --unretire_jobs --batch_system "condor" #--with_skimmed #--short #
